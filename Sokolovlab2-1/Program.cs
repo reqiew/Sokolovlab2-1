9f685fcf5d249795cc158d28f248e56a22b209eb
dobry <dobry@DESKTOP-FBD3PAQ>
2023-09-28 22:33:25 +0300

    Добавьте файлы проекта.

---
+++ Sokolovlab2-1/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите k");
+int k = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите j");
+int j = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите n");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите m");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine(((k == 0) && (j > m))||((k < 0) && (2 * j - 3 * n) < m));
